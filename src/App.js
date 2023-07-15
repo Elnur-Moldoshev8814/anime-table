65d1b8282aebb72e1bdb8a71a278013b4d1e1b84
El1488 <118663738+Elnur-Moldoshev8814@users.noreply.github.com>
2023-07-15 18:02:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import logo from './logo.svg'; 
+import './App.css'; 
+ 
+function App() { 
+  return ( 
+    <> 
+      <div className='container'> 
+        <div className='wrappeer'> 
+          <div className='card'> 
+ 
+            <img className='img' src="https://static.hdrezka.ac/i/2016/10/18/l451e99fa2fdbbu69i21i.jpg" alt="Торадора!"/> 
+ 
+            <div className='text'> 
+              <p className='title'>Торадора!</p> 
+              <p className='subtitle'>Семнадцатилетний парень Рюдзи, которого все боятся из-за страшного взгляда, влюбляется в одноклассницу Минори, а его соседка Тайга влюбляется в Китамару. Они решают объединить свои усилия, но может быть они просто не в тех влюбились?</p> 
+            </div> 
+             
+            <a href="https://ru.wikipedia.org/wiki/%D0%A2%D0%BE%D1%80%D0%B0%D0%B4%D0%BE%D1%80%D0%B0!"> 
+              <button className='btn'>more information</button>  
+            </a> 
+ 
+          </div> 
+ 
+          <div className='card'> 
+ 
+            <img className='img' src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/51decfa7-9154-4904-8c8f-762bac4eb6a8/600x900" alt="K-ON!"/> 
+ 
+            <div className='text'> 
+              <p className='title'>K-ON!</p> 
+              <p className='subtitle'>Любите ли вы музыку? Начинающая гитаристка Юи Хирасава определённо да. Спасая школьный музыкальный клуб реот закрытия, она собирает музыкальный коллектив, о котором давно мечтала. Хотя, как сказать, репетируя - скорее попивая чай и болтая о девичьем, попутно выступая на концертах и раскрывая свой творческий потенциал.</p> 
+            </div> 
+             
+            <a href="https://jut.su/k-on/"> 
+              <button className='btn'>more information</button>  
+            </a> 
+ 
+          </div> 
+ 
+          <div className='card'> 
+ 
+            <img className='img' src="https://i1.sndcdn.com/artworks-cWguVYKtrkGq9JRJ-23MfVA-t500x500.jpg" alt="Меланхолия Харухи Судзумии"/> 
+ 
+            <div className='text'> 
+              <p className='title'>Меланхолия Харухи Судзумии</p> 
+              <p className='subtitle'>Девушка мечтает о встрече с инопланетянами, гостями из будущего и желает привлечь внимание секретных спецслужб. Но школьная рутина берёт своё, и Харухи вместе с более заурядным школьником Кёном организует клуб SOS, чьё формальное предназначение - поиски сверхъестественного</p> 
+            </div> 
+             
+            <a href="https://www.kinopoisk.ru/series/407267/"> 
+              <button className='btn'>more information</button>  
+            </a> 
+ 
+          </div> 
+ 
+          <div className='card'> 
+ 
+            <img className='img' src="https://avatars.mds.yandex.net/get-kinopoisk-image/1898899/3dab191b-ca51-4e24-a258-e762fba590c9/220x330" alt="Муми-тролли!"/> 
+ 
+            <div className='text'> 
+              <p className='title'>Муми-тролли</p> 
+              <p className='subtitle'>Му́ми-тро́лли — центральные персонажи серии книг финской шведскоязычной писательницы Туве Янссон. Книги были первоначально изданы на шведском языке, а затем переведены на десятки других языков, включая русский</p> 
+            </div> 
+             
+            <a href="https://ru.wikipedia.org/wiki/%D0%9C%D1%83%D0%BC%D0%B8-%D1%82%D1%80%D0%BE%D0%BB%D0%BB%D0%B8"> 
+              <button className='btn'>more information</button>  
+            </a> 
+ 
+          </div> 
+ 
+          <div className='card'> 
+ 
+            <img className='img' src="https://avatars.mds.yandex.net/get-kinopoisk-image/1898899/3dab191b-ca51-4e24-a258-e762fba590c9/220x330" alt="Муми-тролли!"/> 
+ 
+            <div className='text'> 
+              <p className='title'>Муми-тролли</p> 
+              <p className='subtitle'>Му́ми-тро́лли — центральные персонажи серии книг финской шведскоязычной писательницы Туве Янссон. Книги были первоначально изданы на шведском языке, а затем переведены на десятки других языков, включая русский</p> 
+            </div> 
+             
+            <a href="https://ru.wikipedia.org/wiki/%D0%9C%D1%83%D0%BC%D0%B8-%D1%82%D1%80%D0%BE%D0%BB%D0%BB%D0%B8"> 
+              <button className='btn'>more information</button>  
+            </a> 
+ 
+          </div> 
+           
+        </div> 
+      </div> 
+    </> 
+  ); 
+} 
+ 
+export {App};
